40608cf7bc50eb293b336538ffec8b46f74d3e66
Azeem <azeemtk9@gmail.com>
2023-07-07 19:23:45 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Todo-app',
+      title: 'Todo-App',
       home: Splash(),
     );
   }
